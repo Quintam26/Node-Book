13f544389f0f9d7a47e744f465afa7440af894c2
Mario Quintana <pyrros2000@gmail.com>
2018-07-08 20:16:32 -0700

    completed responding to request handlers with non-blocking operations

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,21 @@
+const exec = require('child_process').exec
+
 function start() {
     console.log('Request handler START was called.');
+    const content = 'empty'
+
+    exec('ls -lah', function(error, stdout, stderr) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write(stdout);
+        response.end();
+    });
 }
 
 function upload() {
     console.log('Request handler UPLOAD was called.');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello Upload');
+    response.end();
 }
 
 exports.start = start;
