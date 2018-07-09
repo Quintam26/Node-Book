8919b24da24f915512c781bc3a97dfd8615475d4
Mario Quintana <pyrros2000@gmail.com>
2018-07-08 19:38:10 -0700

    trying to set up route for request having issues

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const http = require('http');
+const url = require('url');
 
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        const pathName = url.parse(request.url).pathName;
-        console.log('Request for ' + pathName + ' recieved.');
+        let pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' recieved.');
+
+        route(pathname);
+        
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World');
         response.end();
